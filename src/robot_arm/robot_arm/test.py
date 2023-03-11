c7d83b890c564564641d467087d04bdced97c326
Marc Wester <marc.wester@gmail.com>
2023-03-11 13:28:01 -0500

    Clean up

--- src/robot_arm/robot_arm/test.py
+++ src/robot_arm/robot_arm/test.py
@@ -1,3 +1,6 @@
+#
+# 3D printed robot arm controller
+#
 from adafruit_servokit import ServoKit
 import board
 import busio
@@ -6,18 +9,18 @@ import math
 from approxeng.input.selectbinder import ControllerResource
 
 # Calculate servo angles from (x,y,z) end position(mm)
-# Returns theta0 (base angle), theta1(lower arm angle), theta2(upper arm angle)
+# Returns theta1(base angle), theta2(lower arm angle), theta3(upper arm angle)
 #
-def ComputeAngles(x3, y3, z3):
+def ComputeAngles(x, y, z):
 
     a2 = 135
     a3 = 147
     # 1
-    t1 = math.atan(x3 / y3) * 57.297 + 90
+    t1 = math.atan(x / y) * 57.297 + 90
     # 2
-    r1 = math.sqrt(x3 * x3 + y3 * y3)
+    r1 = math.sqrt(x * x + y * y)
     # 3
-    r2 = z3 - 0
+    r2 = z - 0
     # 4
     p2 = math.atan(r2 / r1) * 57.297
     # 5
@@ -30,7 +33,7 @@ def ComputeAngles(x3, y3, z3):
     p3 = math.acos((r3 * r3 - a2 * a2 - a3 * a3) / (-2 * a2 * a3)) * 57.297
     # 9
     t3 = 180 - p3
-    t3 += 90 - t2  # compensate for lower arm movement
+    t3 += 90 - t2  # adjust for lower arm linkage to upper arm
 
     # print(f"x: {x}, y: {y}, z: {z}, p1: {p1}, p2: {p2}, p3: {p3} t1: {t1}, t2: {t2}, t3: {t3}")
 
@@ -63,24 +66,25 @@ try:
             print(type(joystick).__name__)
             joystick.check_presses()
             while joystick.connected:
-                axis_list = ["l", "lt", "lx", "ly", "r", "rt", "rx", "ry"]
+                # axis_list = ["l", "lt", "lx", "ly", "r", "rt", "rx", "ry"]    # Full analog control list
+                axis_list = ["lx", "ly", "rt", "ry"]    # Controls used
                 for axis_name in axis_list:
                     joystick_value = joystick[axis_name]
 
                     # X - Axis
-                    if axis_name == "lx":
+                    if axis_name == "lx":   # Left stick X
                         x = joystick_value * 100
 
                     # Y - Axis
-                    if axis_name == "ry":
+                    if axis_name == "ry":   # Right stick Y
                         y = -joystick_value * 100.0 + 147
 
                     # Z - Axis
-                    if axis_name == "ly":
+                    if axis_name == "ly":   # Left stick Y
                         z = -joystick_value * 100.0 + 107
 
                     # Gripper
-                    if axis_name == "rt":
+                    if axis_name == "rt":   # Right trigger
                         desired_angle = (1 - joystick_value) * 180
                         desired_angle = max(min(desired_angle, servo_limits_max[GRIPPER]), servo_limits_min[GRIPPER])
                         kit.servo[GRIPPER].angle = desired_angle
@@ -93,7 +97,7 @@ try:
                 kit.servo[LOWER_ARM].angle = max(min(t_lower_arm, servo_limits_max[LOWER_ARM]), servo_limits_min[LOWER_ARM])
                 kit.servo[UPPER_ARM].angle = max(min(t_upper_arm, servo_limits_max[UPPER_ARM]), servo_limits_min[UPPER_ARM])
                 # print(f"x: {x:.2f}, y: {y:.2f}, z: {z:.2f}, t1: {t_base:.2f}, t2: {t_lower_arm:.2f}, t3: {t_upper_arm:.2f}")
-                time.sleep(0.1)
+                # time.sleep(0.1)
 
                 # button_list = [
                 #     "circle",
